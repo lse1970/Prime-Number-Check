b3b69b1e5eaaa0c832aa3ce3ff7bdc0618806bde
D7702_09 <d7702_09@D7702-09ui-iMac.local>
2018-03-29 16:17:35 +0900

    prime number check

--- Prime Number Check/ViewController.swift
+++ Prime Number Check/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var txtF: UITextField!
+    @IBAction func btC(_ sender: Any) {
+        var number = Int (txtF.text!)
+        var isPrime = true
+        
+        if number == 1  {
+            isPrime = false
+        }
+        
+        if number != 1 && number != 2    {
+            for i in 2 ..< number!   {
+                if number! % i == 0 {
+                    isPrime = false
+                }
+            }
+        }
+        if isPrime == true  {
+            lbl.text = "Prime Number"
+            
+        }else   {
+            
+            lbl.text = "Not Prime Number"
+        }
+    }
+    @IBAction func btR(_ sender: Any) {
+        txtF.text = ""
+        lbl.text = ""
+    }
+    @IBOutlet weak var lbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        txtF.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        view.endEditing(false)
+        return true
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(false)
+    }
 
 }
 
